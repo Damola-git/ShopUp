762f00769c24433c8ab29b5c2cc648bc5f8891b2
Adejumo Adedamola <damoladavidadejumo@gmail.com>
2025-10-13 18:16:47 +0100

    first commit

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   final productId = ModalRoute.of(context).settings.arguments as String
+   final loadedProduct = provider.
+  }
+}
