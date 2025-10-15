3e828a65cfcdb224f70f8bfe396a793f7af14aad
Adejumo Adedamola <damoladavidadejumo@gmail.com>
2025-10-15 09:29:36 +0100

    update

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -8,6 +8,7 @@ class ProductsGrid extends StatelessWidget {
    final bool showFavs;
 
   ProductsGrid(this.showFavs);
+  
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
